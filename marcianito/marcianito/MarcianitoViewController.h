5285b1bcbf1688724690ea79e9ee78446b7e2a6d
Pedro Hernández <pedroh.r99@gmail.com>
2012-10-23 11:09:34 +0100

    Proyecto hecho por Antonio

---
+++ marcianito/marcianito/MarcianitoViewController.h
@@ -0,0 +1,20 @@
+//
+//  MarcianitoViewController.h
+//  marcianito
+//
+//  Created by Lion User on 23/10/12.
+//  Copyright (c) 2012 Lion User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+#import <AudioToolbox/AudioToolbox.h>
+
+@interface MarcianitoViewController : UIViewController
+
+@property (nonatomic, strong) AVAudioPlayer * reproductor;
+@property (nonatomic, strong) IBOutlet UIButton *alienProp;
+
+-(IBAction)alien:(id)sender;
+
+@end
---
+++ marcianito/marcianito/MarcianitoViewController.m
@@ -0,0 +1,91 @@
+//
+//  MarcianitoViewController.m
+//  marcianito
+//
+//  Created by Lion User on 23/10/12.
+//  Copyright (c) 2012 Lion User. All rights reserved.
+//
+
+#import "MarcianitoViewController.h"
+
+@interface MarcianitoViewController ()
+
+@end
+
+@implementation MarcianitoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	NSError * error;
+    NSString * ruta = [[NSBundle mainBundle] pathForResource:@"sonidoFondo" ofType:@"mp3"];
+    
+    NSURL * url = [[NSURL alloc] initFileURLWithPath:ruta];
+    
+    self.reproductor = [[AVAudioPlayer alloc] initWithContentsOfURL:url error: &error];
+    
+    self.reproductor.pan = 0;
+    self.reproductor.volume = 1;
+    
+    self.reproductor.enableRate = NO; //no ralentiza el audio al comenzar
+    self.reproductor.rate = 0;
+    
+    [self.reproductor play];
+    [self.reproductor setNumberOfLoops:-1];//el valor -1 indica que la repetición del bucle de sonido será infinita.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Salto
+
+-(IBAction)alien:(id)sender{
+    
+    SystemSoundID soundID;
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"saltito" ofType:@"mp3"];
+    
+    AudioServicesCreateSystemSoundID((__bridge_retained CFURLRef)[NSURL fileURLWithPath:path],&soundID);
+    
+    AudioServicesPlaySystemSound (soundID);
+    
+    [UIView animateWithDuration:0.5 animations:^(void){
+        
+        //alienProp hace referencia al alien que actúa como botón
+        self.alienProp.center = CGPointMake(self.alienProp.center.x , self.alienProp.center.y -50);
+        
+    }
+                     completion:^(BOOL Finished){
+                         
+                         [self baja];//llamo a baja donde hace lo contrario que el código de subida.
+                         
+                         
+                     }
+     
+     ];
+    
+}
+- (void)baja{
+    
+    [UIView animateWithDuration:0.5 animations:^(void){
+        
+        self.alienProp.center = CGPointMake(self.alienProp.center.x , self.alienProp.center.y +50);
+        
+    }
+                     completion:nil //ya ha llegado abajo así que me quedo nulo
+     ];
+    
+}
+
+@end
